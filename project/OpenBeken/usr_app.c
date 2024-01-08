f61d2e0150dcfe6e8cdd47f1931004e3c3c60a1c
Tester23 <openshwprojects@gmail.com>
2024-01-08 18:48:37 +0100

    it compiles and links

--- project/OpenBeken/usr_app.c
+++ project/OpenBeken/usr_app.c
@@ -184,18 +184,22 @@ void wifi_init_ap(void)
 }
 
 
+void Main_Init();
+
 void usr_app_task_entry(void *params)
 {
     LN_UNUSED(params);
 
     wifi_manager_init();
 
-    wifi_init_sta();
-    // wifi_init_ap();
+   // wifi_init_sta();
+    wifi_init_ap();
+	
+	Main_Init();
 
-    while (!netdev_got_ip()) {
-        OS_MsDelay(1000);
-    }
+    //while (!netdev_got_ip()) {
+    //    OS_MsDelay(1000);
+    //}
     
     while(1)
     {
