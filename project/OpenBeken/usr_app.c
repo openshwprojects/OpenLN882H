46055980136d1e33fb1a9fc62a375e9e8952adc1
Peter Edwards <sweetlilmre@gmail.com>
2024-01-08 21:46:00 +0200

    Added ADC into app HAL

--- project/OpenBeken/usr_app.c
+++ project/OpenBeken/usr_app.c
@@ -5,7 +5,6 @@
 #include "netif/ethernetif.h"
 #include "wifi_manager.h"
 #include "lwip/tcpip.h"
-#include "drv_adc_measure.h"
 #include "utils/debug/ln_assert.h"
 #include "utils/system_parameter.h"
 #include "utils/sysparam_factory_setting.h"
@@ -203,43 +202,6 @@ void usr_app_task_entry(void *params)
     }
 }
 
-void temp_cal_app_task_entry(void *params)
-{
-    LN_UNUSED(params);
-    uint8_t cnt = 0;
-    int8_t cap_comp = 0;
-    uint16_t adc_val = 0;
-    int16_t curr_adc = 0;
-
-    if (NVDS_ERR_OK == ln_nvds_get_xtal_comp_val((uint8_t *)&cap_comp)) {
-        if ((uint8_t)cap_comp == 0xFF) {
-            cap_comp = 0;
-        }
-    }
-
-    drv_adc_init();
-
-    wifi_temp_cal_init(drv_adc_read(ADC_CH0), cap_comp);
-
-    while (1)
-    {
-        OS_MsDelay(1000);
-
-        adc_val = drv_adc_read(ADC_CH0);
-        wifi_do_temp_cal_period(adc_val);
-
-        curr_adc = (adc_val & 0xFFF);
-
-        cnt++;
-        if ((cnt % 60) == 0) {
-            LOG(LOG_LVL_INFO, "adc raw: %4d, temp_IC: %4d\r\n",
-                    curr_adc, (int16_t)(25 + (curr_adc - 770) / 2.54f));
-            LOG(LOG_LVL_INFO, "Total:%d; Free:%ld;\r\n", 
-                    OS_HeapSizeGet(), OS_GetFreeHeapSize());
-        }
-    }
-}
-
 void creat_usr_app_task(void)
 {
     {
@@ -265,12 +227,6 @@ void creat_usr_app_task(void)
     if(OS_OK != OS_ThreadCreate(&g_usr_app_thread, "UsrAPP", usr_app_task_entry, NULL, OS_PRIORITY_BELOW_NORMAL, USR_APP_TASK_STACK_SIZE)) {
         LN_ASSERT(1);
     }
-
-#if  WIFI_TEMP_CALIBRATE
-    if(OS_OK != OS_ThreadCreate(&g_temp_cal_thread, "TempAPP", temp_cal_app_task_entry, NULL, OS_PRIORITY_BELOW_NORMAL, TEMP_APP_TASK_STACK_SIZE)) {
-        LN_ASSERT(1);
-    }
-#endif
 
     /* print sdk version */
     {
